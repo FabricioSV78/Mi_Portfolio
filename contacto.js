62025f83247999d4bc834e672eea421c9f307562
FabricioSV78 <fabricioseminarioo2004@gmail.com>
2025-07-18 23:42:18 -0500

    Mi_Portfolio.

--- contacto.js
+++ contacto.js
@@ -1,29 +1,22 @@
 //email
 
-document.addEventListener("DOMContentLoaded", function() {
+(function(){
   emailjs.init("oqQZyBGYO60eiKK52");
+})();
 
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  document.getElementById('loader').style.display = 'flex'; 
 
-    // Primero: envío de confirmación al cliente
-    emailjs.sendForm('service_72evku6', 'template_h8r0kom', this)
-      .then(function() {
-        console.log("Correo de confirmación enviado al cliente.");
-
-        // Luego: envío de notificación a ti
-        return emailjs.sendForm('service_72evku6', 'template_8se1h3l', event.target);
-      })
-      .then(function() {
-        console.log("Notificación enviada a mi correo.");
-        document.getElementById('formResponse').innerHTML =
-          '<p style="color:#00bcd4;">Mensaje enviado correctamente. ¡Gracias por contactarme!</p>';
-        document.getElementById('contact-form').reset();
-      })
-      .catch(function(error) {
-        console.log("Error al enviar:", error);
-        document.getElementById('formResponse').innerHTML =
-          '<p style="color:red;">Ocurrió un error. Por favor intenta de nuevo.</p>';
-      });
-  });
+  emailjs.sendForm('service_72evku6', 'template_h8r0kom', this)
+    .then(function() {
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('formResponse').innerHTML = '<p style="color:#00bcd4;">Mensaje enviado correctamente. ¡Gracias por contactarme!</p>';
+      document.getElementById('contact-form').reset();
+    }, function(error) {
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('formResponse').innerHTML = '<p style="color:red;">Ocurrió un error. Por favor intenta de nuevo.</p>';
+      console.error('FAILED...', error);
+    });
 });
+
